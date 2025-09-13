c9af01161035b95ad8655520cd804714b613bdcf
domenicodimaio <dimaio-domenico@hotmail.it>
2025-09-13 10:56:45 +0200

    🔥 FIX DEFINITIVO: Rimozione duplicazione BacchusLiveActivity che causava switch must be exhaustive

--- ios/BacchusWidget/BacchusWidgetBundle.swift
+++ ios/BacchusWidget/BacchusWidgetBundle.swift
@@ -13,15 +13,4 @@ struct BacchusWidgetBundle: WidgetBundle {
 }
 
 // MARK: - Live Activity Widget (iOS 16.1+)
-@available(iOS 16.1, *)
-struct BacchusLiveActivity: Widget {
-    var body: some WidgetConfiguration {
-        ActivityConfiguration(for: BacchusActivityAttributes.self) { context in
-            // Lock screen/banner UI
-            BacchusLiveActivityView(context: context)
-        } dynamicIsland: { context in
-            // Dynamic Island UI
-            BacchusLiveActivityDynamicIsland(context: context).body
-        }
-    }
-}
+// RIMOSSO: Duplicazione di BacchusLiveActivity (già definita in BacchusWidgetLiveActivity.swift)
