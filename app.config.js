bf565ba070970dd6b84f20a4b5b810ad0bc52916
domenicodimaio <dimaio-domenico@hotmail.it>
2025-09-30 08:08:05 +0200

    Build: Aggiornamento automatico build number e configurazioni

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.bacchusapp.app',
-      buildNumber: '1462',
+      buildNumber: '1463',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSSupportsLiveActivities: true,
@@ -42,7 +42,7 @@ export default {
     },
     android: {
       package: 'com.bacchusapp.app',
-      versionCode: 1462,
+      versionCode: 1463,
       adaptiveIcon: {
         foregroundImage: './assets/icon.png',
         backgroundColor: '#0c1620'
