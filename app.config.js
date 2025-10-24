c1c998877403f69ff714b67a93a11532c33b79ad
domenicodimaio <dimaio-domenico@hotmail.it>
2025-10-24 19:57:41 +0200

    Build: Aggiornato build number per nuova build

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.bacchusapp.app',
-      buildNumber: '2516',
+      buildNumber: '2517',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
         NSSupportsLiveActivities: true,
@@ -42,7 +42,7 @@ export default {
     },
     android: {
       package: 'com.bacchusapp.app',
-      versionCode: 2516,
+      versionCode: 2517,
       adaptiveIcon: {
         foregroundImage: './assets/icon.png',
         backgroundColor: '#0c1620'
