c7b3064e043be75690b1cc90defe011b47c7e483
domenicodimaio <dimaio-domenico@hotmail.it>
2025-11-24 11:37:04 +0100

    Build: Aggiornato build number per nuova build

--- app.config.js
+++ app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: false, // 🔄 APPLE FEEDBACK: iPhone-only ma che funzioni bene quando upscalato su iPad
       bundleIdentifier: 'com.bacchusapp.app',
-      buildNumber: '2811',
+      buildNumber: '2812',
       infoPlist: {
         UIDeviceFamily: [1], // 1 = iPhone only - ma layout responsive per upscaling iPad
         UIRequiresFullScreen: true, // iPhone app in modalità fullscreen
@@ -47,7 +47,7 @@ export default {
     },
       android: {
         package: 'com.bacchusapp.app',
-        versionCode: 2811,
+        versionCode: 2812,
       adaptiveIcon: {
         foregroundImage: './assets/icon.png',
         backgroundColor: '#0c1620'
